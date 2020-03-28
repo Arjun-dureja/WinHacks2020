b88a4633fd2924accca153ecbfbf6f7417f65acf
Arjun Dureja <arjun.dureja1000@gmail.com>
2020-03-28 16:01:07 -0400

    webapp

---
+++ WinHacks2020/build/precache-manifest.4c324038a1f3c6051a882f10bcb89b0f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b072d32dabff435113cdf84603e1a19c",
+    "url": "/material-dashboard-react/index.html"
+  },
+  {
+    "revision": "7259bf5f8f994a347066",
+    "url": "/material-dashboard-react/static/css/2.d0176e96.chunk.css"
+  },
+  {
+    "revision": "bb57eeb895ff62834035",
+    "url": "/material-dashboard-react/static/css/main.1a5f11bd.chunk.css"
+  },
+  {
+    "revision": "7259bf5f8f994a347066",
+    "url": "/material-dashboard-react/static/js/2.8bd7c5c9.chunk.js"
+  },
+  {
+    "revision": "bb57eeb895ff62834035",
+    "url": "/material-dashboard-react/static/js/main.078dca7b.chunk.js"
+  },
+  {
+    "revision": "dd1fc649e6c806df14cf",
+    "url": "/material-dashboard-react/static/js/runtime~main.05dc72a2.js"
+  },
+  {
+    "revision": "8880a65c57d7f031579335be153f64a0",
+    "url": "/material-dashboard-react/static/media/marc.8880a65c.jpg"
+  },
+  {
+    "revision": "8e9f0a3c5578a20733d5bad0e51c91fb",
+    "url": "/material-dashboard-react/static/media/sidebar-1.8e9f0a3c.jpg"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/material-dashboard-react/static/media/sidebar-2.310509c9.jpg"
+  },
+  {
+    "revision": "2503169017014036cf0dd0dd7c2a2b8a",
+    "url": "/material-dashboard-react/static/media/sidebar-3.25031690.jpg"
+  },
+  {
+    "revision": "fc9cb0538eb5a4dfd6fbb1bf6dd6189b",
+    "url": "/material-dashboard-react/static/media/sidebar-4.fc9cb053.jpg"
+  }
+]);
